b07a4618cc48d4cdc2fb62b509e21527fd4c292f
Yoyojesus <austinplaysmms12@gmail.com>
2026-02-05 15:46:41 -0500

    needed vercel.json file

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -4,20 +4,13 @@ import adapter from '@sveltejs/adapter-static';
 const config = {
 	compilerOptions: {
 		warningFilter: (warning) => {
-			// Ignore a11y warnings for labels in dynamic forms
 			if (warning.code === 'a11y_label_has_associated_control') return false;
 			if (warning.code === 'a11y_invalid_attribute') return false;
 			return true;
 		}
 	},
 	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
-			precompress: false,
-			strict: true
-		})
+		adapter: adapter()
 	}
 };
 
